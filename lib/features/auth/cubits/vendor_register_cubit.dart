0113a563641238dcb3b419338139af14b425ea89
EslamKamel89 <elsamkamelforex@gmail.com>
2025-09-17 18:25:03 +0300

    feat(auth): add vendor register cubit - Handle registration, cache user, and logout

---
+++ lib/features/auth/cubits/vendor_register_cubit.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gps_app/core/cache/cache_keys.dart';
+import 'package:gps_app/core/cache/local_storage.dart';
+import 'package:gps_app/core/enums/response_type.dart';
+import 'package:gps_app/core/models/api_response_model.dart';
+import 'package:gps_app/core/service_locator/service_locator.dart';
+import 'package:gps_app/features/auth/models/vendor_register_params/vendor_register_params.dart';
+
+import '../controllers/auth_controller.dart';
+import '../models/user_model.dart';
+
+class VendorRegisterCubit extends Cubit<ApiResponseModel<UserModel>> {
+  VendorRegisterCubit() : super(ApiResponseModel());
+
+  final _controller = serviceLocator<AuthController>();
+  final _storage = serviceLocator<LocalStorage>();
+
+  Future<void> register({required VendorRegisterParams param}) async {
+    emit(state.copyWith(response: ResponseEnum.loading));
+    final res = await _controller.vendorRegister(param: param);
+    if (res.response == ResponseEnum.success && res.data != null) {
+      final jsonStr = jsonEncode(res.data!.toJson());
+      await _storage.setString(CacheKeys.userJson, jsonStr);
+    }
+    emit(res);
+  }
+
+  UserModel? get cachedUser {
+    final raw = _storage.getString(CacheKeys.userJson);
+    if (raw == null) return null;
+    try {
+      return UserModel.fromJson(jsonDecode(raw));
+    } catch (_) {
+      return null;
+    }
+  }
+
+  Future<void> logout() async {
+    await _storage.remove(CacheKeys.userJson);
+    emit(ApiResponseModel());
+  }
+}
