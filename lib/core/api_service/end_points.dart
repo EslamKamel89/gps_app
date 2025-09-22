b81246491193a131395f11c64fb9ddd296478edb
EslamKamel89 <elsamkamelforex@gmail.com>
2025-09-22 17:12:18 +0300

    feat(auth): add holiday API support - Add holidays endpoint and model - Implement holidays fetch in AuthController - Introduce HolidayCubit for state management

--- lib/core/api_service/end_points.dart
+++ lib/core/api_service/end_points.dart
@@ -3,11 +3,11 @@ class EndPoint {
   // static const String baseUrl = "http://127.0.0.1:8000";
   static const String category = "$baseUrl/api/category";
   static const String states = "$baseUrl/api/states";
+  static const String holidays = "$baseUrl/api/holidays";
   static const String districts = "$baseUrl/api/districts";
   static const String userRegister = "$baseUrl/api/user-register";
   static const String vendorRegister = "$baseUrl/api/vendor-register";
   static const String login = "$baseUrl/api/login";
   static const String uploadImages = "$baseUrl/api/uploads/images";
-  static const String vendorCatalogSection =
-      "$baseUrl/api/vendors/catalog/bulk";
+  static const String vendorCatalogSection = "$baseUrl/api/vendors/catalog/bulk";
 }
---
+++ lib/features/auth/cubits/holiday_cubit.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gps_app/core/enums/response_type.dart';
+import 'package:gps_app/core/helpers/print_helper.dart';
+import 'package:gps_app/core/models/api_response_model.dart';
+import 'package:gps_app/core/service_locator/service_locator.dart';
+import 'package:gps_app/features/auth/controllers/auth_controller.dart';
+import 'package:gps_app/features/auth/models/holiday_model.dart';
+
+class HolidayCubit extends Cubit<ApiResponseModel<List<HolidayModel>>> {
+  HolidayCubit() : super(ApiResponseModel());
+  final AuthController controller = serviceLocator<AuthController>();
+  Future holidaysIndex() async {
+    final t = prt('holidaysIndex - HolidayCubit');
+    emit(state.copyWith(errorMessage: null, response: ResponseEnum.loading));
+    final ApiResponseModel<List<HolidayModel>> response = await controller.holidays();
+    pr(response, t);
+    emit(state);
+  }
+}
