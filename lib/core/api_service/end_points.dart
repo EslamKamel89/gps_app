da98f66f8d490e807fa6e67f4a176cf22662cb09
EslamKamel89 <elsamkamelforex@gmail.com>
2025-09-27 08:02:05 +0300

    chore: update .gitignore and API endpoint; add todo.json - Remove todo.json from .gitignore - Switch API base URL to production - Add todo.json with task tracking

--- lib/core/api_service/end_points.dart
+++ lib/core/api_service/end_points.dart
@@ -1,6 +1,7 @@
 class EndPoint {
-  static const String baseUrl = "http://10.0.2.2:8000";
+  // static const String baseUrl = "http://10.0.2.2:8000";
   // static const String baseUrl = "http://127.0.0.1:8000";
+  static const String baseUrl = "https://gps.gaztec.org";
   static const String category = "$baseUrl/api/category";
   static const String states = "$baseUrl/api/states";
   static const String holidays = "$baseUrl/api/holidays";
