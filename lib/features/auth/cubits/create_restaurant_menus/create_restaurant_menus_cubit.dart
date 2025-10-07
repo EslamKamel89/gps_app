de42be7b33f7c888d506dd670790a89551e37a22
EslamKamel89 <elsamkamelforex@gmail.com>
2025-10-07 09:58:35 +0300

    feat(auth): add restaurant menus cubit and clean up catalog cubit - Implement CreateRestaurantMenusCubit with meal/menu management

---
+++ lib/features/auth/cubits/create_restaurant_menus/create_restaurant_menus_cubit.dart
@@ -0,0 +1,57 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gps_app/core/enums/response_type.dart';
+import 'package:gps_app/core/helpers/print_helper.dart';
+import 'package:gps_app/core/models/api_response_model.dart';
+import 'package:gps_app/core/service_locator/service_locator.dart';
+import 'package:gps_app/features/auth/controllers/auth_controller.dart';
+import 'package:gps_app/features/auth/models/menu_param/meal.dart';
+import 'package:gps_app/features/auth/models/menu_param/menu_param.dart';
+
+part 'create_restaurant_menus_state.dart';
+
+class CreateRestaurantMenusCubit extends Cubit<CreateRestaurantMenusState> {
+  final controller = serviceLocator<AuthController>();
+  CreateRestaurantMenusCubit() : super(CreateRestaurantMenusState.initial());
+
+  void addMeal({required MenuParam menuParam, required MealParam mealParam}) {
+    menuParam.meals ??= [];
+    menuParam.meals!.add(mealParam);
+    emit(state.copyWith());
+  }
+
+  void removeMeal({required MenuParam menuParam, required MealParam mealParam}) {
+    menuParam.meals ??= [];
+    menuParam.meals!.remove(mealParam);
+    emit(state.copyWith());
+  }
+
+  void addMenu({required MenuParam menuParam}) {
+    state.menus.add(menuParam);
+    emit(state.copyWith());
+  }
+
+  void removeMenu({required MenuParam menuParam}) {
+    state.menus.remove(menuParam);
+    emit(state.copyWith());
+  }
+
+  Future createMenus() async {
+    final t = prt('createMenus - CreateRestaurantMenusCubit');
+    // pr(state.sections, 'state.sections');
+    // return;
+    emit(
+      state.copyWith(
+        menusResponse: state.menusResponse.copyWith(
+          errorMessage: null,
+          response: ResponseEnum.loading,
+          data: null,
+        ),
+      ),
+    );
+    final ApiResponseModel<bool> response = await controller.createRestaurantMenus(
+      param: state.menus,
+    );
+    pr(response, t);
+    emit(state.copyWith(menusResponse: response));
+  }
+}
---
+++ lib/features/auth/cubits/create_restaurant_menus/create_restaurant_menus_state.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+part of 'create_restaurant_menus_cubit.dart';
+
+class CreateRestaurantMenusState {
+  ApiResponseModel<bool> menusResponse;
+  List<MenuParam> menus;
+  CreateRestaurantMenusState({required this.menusResponse, required this.menus});
+
+  CreateRestaurantMenusState copyWith({
+    ApiResponseModel<bool>? menusResponse,
+    List<MenuParam>? menus,
+  }) {
+    return CreateRestaurantMenusState(
+      menusResponse: menusResponse ?? this.menusResponse,
+      menus: menus ?? this.menus,
+    );
+  }
+
+  @override
+  String toString() => 'CreateRestaurantMenusState(menusResponse: $menusResponse, menus: $menus)';
+
+  factory CreateRestaurantMenusState.initial() {
+    return CreateRestaurantMenusState(
+      menus: [
+        MenuParam(meals: [MealParam()]),
+      ],
+      menusResponse: ApiResponseModel(),
+    );
+  }
+}
